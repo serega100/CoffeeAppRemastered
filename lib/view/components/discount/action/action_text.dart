ae7894ad40d577504900fe7ffaa39de07d5364d6
Sergey Kazantsev <serzh.kazantseff@gmail.com>
2022-01-01 21:32:09 +0500

    Correct discount_container_typography.dart name

--- lib/view/components/discount/action/action_text.dart
+++ lib/view/components/discount/action/action_text.dart
@@ -1,5 +1,5 @@
 import 'package:coffee_app_remastered/view/components/discount/action/i_discount_action.dart';
-import 'package:coffee_app_remastered/view/components/discount/discount_contatiner_typography.dart';
+import 'package:coffee_app_remastered/view/components/discount/discount_container_typography.dart';
 import 'package:flutter/material.dart';
 
 class DiscountActionText extends StatelessWidget implements IDiscountAction {
