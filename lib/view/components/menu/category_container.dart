8b1e9c7078a88fc8e3d6e22de43ea878ca566ab2
Sergey Kazantsev <serzh.kazantseff@gmail.com>
2022-01-09 03:12:50 +0500

    Beautify category_container.dart code

--- lib/view/components/menu/category_container.dart
+++ lib/view/components/menu/category_container.dart
@@ -2,7 +2,6 @@ import 'package:coffee_app_remastered/model/product/category.dart';
 import 'package:coffee_app_remastered/model/product/product.dart';
 import 'package:coffee_app_remastered/view/components/menu/product_container.dart';
 import 'package:coffee_app_remastered/view/view_settings.dart';
-import 'package:coffee_app_remastered/view/view_utils.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:google_fonts/google_fonts.dart';
@@ -36,28 +35,18 @@ class CategoryContainer extends StatelessWidget {
         SizedBox(height: 20),
         Wrap(
           runSpacing: 20.0,
-          children: _getProductContainers(products),
+          children: [
+            for (var product in products)
+              ProductContainer(
+                image: product.image,
+                title: product.title,
+                volumeString: product.volumeString,
+                price: product.price,
+                onAdded: () => onProductAdded(product),
+              )
+          ],
         ),
       ],
     );
-  }
-
-  List<ProductContainer> _getProductContainers(List<Product> list) {
-    var containers = <ProductContainer>[];
-    for (var product in list) {
-      var volume = product.volume;
-      var units = product.volumeUnits;
-      var volumeStr = ViewUtils.beautifyVolume(volume, units);
-
-      containers.add(ProductContainer(
-        image: product.image,
-        title: product.title,
-        volumeString: volumeStr,
-        price: product.price,
-        onAdded: () => onProductAdded(product),
-      ));
-    }
-
-    return containers;
   }
 }
