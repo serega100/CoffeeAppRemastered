6ab0f54bcd56829b0573abc19a31b30578be17ba
Sergey Kazantsev <serzh.kazantseff@gmail.com>
2022-01-28 18:11:16 +0500

    Wrap with InkWell AddProductButton

--- lib/view/components/menu/add_product_button.dart
+++ lib/view/components/menu/add_product_button.dart
@@ -1,3 +1,4 @@
+import 'package:coffee_app_remastered/view/view_settings.dart';
 import 'package:flutter/material.dart';
 
 class AddProductButton extends StatefulWidget {
@@ -15,11 +16,22 @@ class AddProductButton extends StatefulWidget {
 class _AddProductButtonState extends State<AddProductButton> {
   @override
   Widget build(BuildContext context) {
-    return GestureDetector(
-      onTap: widget.onTap,
-      child: Center(
-        child: Icon(Icons.add_box, color: Color(0xFF6B453C), size: 40.0),
-      )
+    return ClipRRect(
+      borderRadius: const BorderRadius.all(Radius.circular(5)),
+      child: Container(
+        width: 35,
+        height: 35,
+        color: ViewSettings.secondColor,
+        child: Material(
+          color: Colors.transparent,
+          child: InkWell(
+            onTap: widget.onTap,
+            child: const Center(
+              child: Icon(Icons.add, color: Colors.white, size: 30.0),
+            ),
+          ),
+        ),
+      ),
     );
   }
 }
